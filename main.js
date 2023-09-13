6e2cdce55be5c156821cf58bf5e71fec0cb4117b
KotaTashiro <kota.tashiro@stunited.vn>
2023-09-13 16:34:24 +0700

    finished sidebar

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+const pics_src = ["/image/side-pucture1.png","/image/side-pucture2.png","/image/side-pucture3.png","/image/side-pucture4.png","/image/side-pucture5.png","/image/side-pucture6.png"]
+
+// 上に戻るボタン
 document.getElementById("go-top").onclick = function() {
     window.scrollTo(0,0);
 };
 
+// サイドバーボタン開く
 const side_bar_button = document.getElementById("side-bar-button");
 const side_bar = document.getElementById("side-bar");
 
@@ -10,124 +14,109 @@ side_bar_button.addEventListener('click', () => {
   side_bar.classList.remove('hidden');
 });
 
+// サイドバーボタン閉じる
 const side_bar_button_back = document.getElementById("side_bar_button_back");
 side_bar_button_back.addEventListener('click', () => {
     side_bar.classList.add('hidden');
 });
 
-const display_image1 = document.getElementById("dispplay-image1");
-const image_display1 = document.getElementById("image-display1");
 
+// 画像ポップアップ画面処理
+const display_image = document.getElementById("dispplay-image");
+const image_display = document.getElementById("image-display");
+const image_back_button = document.getElementById("image-backbutton") ;
 
+const mypic = document.getElementById("mypic");
+
+// 画像ポップアップ画面非表示処理
+image_back_button.addEventListener('click', () => {
+    // ボタンクリックでhiddenクラスを付け外しする
+    image_display.classList.add('hidden');
+});
+
+// 画像ポップアップ画面表示処理
+const image1 = document.getElementById("image-1");
 image1.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display1.classList.remove('hidden');
+    mypic.src = image1.src
+    mypic.setAttribute("image-id", 0)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button1 = document.getElementById("image-backbutton1");
-
-image_back_button1.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display1.classList.add('hidden');
-});
-
-const image2 = document.getElementById("image2");
-const image_display2 = document.getElementById("image-display2");
-
+const image2 = document.getElementById("image-2");
 image2.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display2.classList.remove('hidden');
+    mypic.src = image2.src
+    mypic.setAttribute("image-id", 1)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button2 = document.getElementById("image-backbutton2");
-
-image_back_button2.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display2.classList.add('hidden');
-      num = 0;
-});
-
-const image3 = document.getElementById("image3");
-const image_display3 = document.getElementById("image-display3");
-
+const image3 = document.getElementById("image-3");
 image3.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display3.classList.remove('hidden');
+    mypic.src = image3.src
+    mypic.setAttribute("image-id", 2)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button3 = document.getElementById("image-backbutton3");
-
-image_back_button3.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display3.classList.add('hidden');
-});
-
-const image4 = document.getElementById("image4");
-const image_display4 = document.getElementById("image-display4");
-
+const image4 = document.getElementById("image-4");
 image4.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display4.classList.remove('hidden');
+    mypic.src = image4.src
+    mypic.setAttribute("image-id", 3)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button4 = document.getElementById("image-backbutton4");
-
-image_back_button4.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display4.classList.add('hidden');
-});
-
-const image5 = document.getElementById("image5");
-const image_display5 = document.getElementById("image-display5");
-
+const image5 = document.getElementById("image-5");
 image5.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display5.classList.remove('hidden');
+    mypic.src = image5.src
+    mypic.setAttribute("image-id", 4)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button5 = document.getElementById("image-backbutton5");
-
-image_back_button5.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display5.classList.add('hidden');
-});
-
-const image6 = document.getElementById("image6");
-const image_display6 = document.getElementById("image-display6");
-
+const image6 = document.getElementById("image-6");
 image6.addEventListener('click', () => {
     //   // ボタンクリックでhiddenクラスを付け外しする
-    image_display6.classList.remove('hidden');
+    mypic.src = image6.src
+    mypic.setAttribute("image-id", 5)
+    image_display.classList.remove('hidden');
 });
 
-const image_back_button6 = document.getElementById("image-backbutton6");
-
-image_back_button6.addEventListener('click', () => {
-    //   // ボタンクリックでhiddenクラスを付け外しする
-      image_display6.classList.add('hidden');
-});
-
-
-var pics_src = new Array("/image/side-pucture1.png","/image/side-pucture2.png","/image/side-pucture3.png","/image/side-pucture4.png","/image/side-pucture5.png","/image/side-pucture6.png");
-
-var num = 1;
-
+// ポップアップ画面切り替え関数
 function go_forward(){
-    if (num == 5) {
-        num = 0;
+    const imageId = Number(mypic.getAttribute("image-id"));
+    let imageNumber = imageId + 1;
+
+    if (imageNumber >= 6) {
+        imageNumber = 0;
     }
-    else {
-        num ++;
-    }
-    document.getElementById("mypic").src=pics_src[num];
+
+    console.log(imageNumber);
+    mypic.src = pics_src[imageNumber];
+    mypic.setAttribute("image-id", imageNumber);
 }
 
 function go_back(){
-    if (num == 0) {
-        num = 5;
+    const imageId = Number(mypic.getAttribute("image-id"));
+    let imageNumber = imageId - 1;
+
+    if (imageNumber <= -1) {
+        imageNumber = 5;
     }
-    else {
-        num --;
-    }
-    document.getElementById("mypic").src=pics_src[num];
+
+    console.log(imageNumber);
+    mypic.src = pics_src[imageNumber];
+    mypic.setAttribute("image-id", imageNumber);
 }
+
+
+const attention1 = document.getElementById("attention1");
+const attention1_pop = document.getElementById("attention1-pop");
+
+attention1.addEventListener('click', () => {
+    //   // ボタンクリックでhiddenクラスを付け外しする
+    attention1_pop.classList.toggle('hidden');
+});
+
+
